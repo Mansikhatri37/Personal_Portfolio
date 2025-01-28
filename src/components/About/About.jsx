41301822d2d076082ba6524d69f17b2cac4492b9
mansikhatri37 <mansikhatri2020@gmail.com>
2025-01-28 20:32:57 +0530

    Initial commit of edited project

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -37,10 +37,9 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>React Native Developer</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                I have experience in developing mobile apps using React Native.
               </p>
             </div>
           </li>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,10 +7,9 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Mansi</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+        I'm a dedicated computer science enthusiast with experience in MERN stack and data structures
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
