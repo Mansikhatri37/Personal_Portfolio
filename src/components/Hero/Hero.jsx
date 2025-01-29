d13150550736949411c476597b5fec7f29748ead
mansikhatri37 <mansikhatri2020@gmail.com>
2025-01-29 15:21:18 +0530

    changed pic

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,14 +9,15 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Mansi</h1>
         <p className={styles.description}>
-        I'm a dedicated computer science enthusiast with experience in MERN stack and data structures
+          I'm a dedicated computer science enthusiast with experience in MERN
+          stack and data structures
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/mansi_pic1.jpg")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
